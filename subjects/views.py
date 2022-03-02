1eb7a6e24c90e011c02700209e257e6d13cf24fb
sami92news <sami.akram@92newshd.tv>
2022-03-02 16:45:19 +0500

    first

---
+++ subjects/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+)
+from .models import Subject
+# Create your views here.
+
+class PostListView(ListView):
+    model = Subject
+    template_name = 'subjects/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'subjects'
+    
+
---
+++ users/models.py
@@ -0,0 +1,24 @@
+from os.path import exists
+
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='/static/users/default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        if self.image:
+            path_to_file = self.image.path
+            if exists(path_to_file):
+                img = Image.open(path_to_file)
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
